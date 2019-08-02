21634bd44f2c8b13c667ef3dd26598c957b48dd2
andre <andre.palhares@dtidigital.com.br>
2019-08-02 08:43:18 -0300

    press events

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+
+export default class App extends Component {
+  state = {
+    places: []
+  };
+
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
+      };
+    });
+  };
+
+  placeDeleteHandler = (index) => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
+      };
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Something has changed</Text>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+        places={this.state.places}
+        onItemDeleted={this.placeDeleteHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: "#DC143C",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
+});
---
+++ src/components/PlaceInput/PlaceInput.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+
+class PlaceInput extends Component {
+  state = {
+    placeName: ""
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.props.onPlaceAdded(this.state.placeName);
+  };
+
+  render() {
+    return (
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter a place"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+          style={styles.placeInput}
+        />
+        <Button
+          title="Add"
+          style={styles.placeButton}
+          onPress={this.placeSubmitHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
+});
+
+export default PlaceInput;
